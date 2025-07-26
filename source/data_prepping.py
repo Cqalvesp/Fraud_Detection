0da65f980d0049eaddbf82b8d72635a1e18c11e2
Christian 🅰️🅿️ <cqalvesp@gmail.com>
2025-07-26 18:42:10 -0400

    data prep for model

---
+++ source/data_prepping.py
@@ -0,0 +1,7 @@
+# Data preparation for ML Model
+import pandas as pd
+
+creditcard = pd.read_csv('C:\Users\cqalv\Documents\Projects\Fraud_Detection\data\creditcard.csv')
+
+def data_prep():
+    pass
--- source/model.py
+++ source/model.py
@@ -1,3 +1,5 @@
 # Machine Learning Model
 
+# MLP Neural Network
+
 import torch
