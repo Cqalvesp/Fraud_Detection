7d94ef3904f3eee8c413d407915dcb525e328be6
Christian 🅰️🅿️ <cqalvesp@gmail.com>
2025-07-27 01:36:40 -0400

    data analysis & prep for model

--- source/data_prepping.py
+++ source/data_prepping.py
@@ -1,5 +1,6 @@
 # Data preparation for ML Model
 import pandas as pd
+import numpy as np
 
 creditcard = pd.read_csv('C:\Users\cqalv\Documents\Projects\Fraud_Detection\data\creditcard.csv')
 
---
+++ source/eda.py
@@ -0,0 +1,14 @@
+# Script to visualize important traits of the dataset
+
+import pandas as pd
+import numpy as np
+
+import matplotlib as mlp
+import seaborn as sns
+import warnings
+
+warnings.filterwarnings("ignore")
+sns.set_theme(style="whitegrid")
+
+df = pd.read_csv('C:\Users\cqalv\Documents\Projects\Fraud_Detection\data\creditcard.csv')
+
