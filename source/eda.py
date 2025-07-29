efab65831a4a364f8aea6efde8e17262ed7a009d
Christian 🅰️🅿️ <cqalvesp@gmail.com>
2025-07-29 01:32:44 -0400

    starting with bar chart to measure class balance

--- source/eda.py
+++ source/eda.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 import matplotlib as mlp
 import seaborn as sns
@@ -10,5 +11,7 @@ import warnings
 warnings.filterwarnings("ignore")
 sns.set_theme(style="whitegrid")
 
-df = pd.read_csv('C:\Users\cqalv\Documents\Projects\Fraud_Detection\data\creditcard.csv')
+df = pd.read_csv('C:/Users/cqalv/Documents/Projects/Fraud_Detection/data/creditcard.csv')
+
+
 
